952a4d42f43d7ffa14a363332335be26bb26fa12
peiling <e1526692@u.nus.edu>
2026-08-21 15:50:07 +0800

    Add Level 0

---
+++ src/main/java/Isa.java
@@ -0,0 +1,8 @@
+public class Isa {
+    public static void main(String[] args) {
+        System.out.println("Helloo! I'm Isa");
+        System.out.println("How can I help you?");
+        System.out.println();
+        System.out.println("Bye. Hope you have a nice day!");
+    }
+}

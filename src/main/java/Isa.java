55d0327bb2f43ec9271551ef2274d3a76a2d5c2f
peiling <e1526692@u.nus.edu>
2026-09-01 15:45:19 +0800

    Add Level 4

--- src/main/java/Isa.java
+++ src/main/java/Isa.java
@@ -1,23 +1,27 @@
+// Isa.java
 import java.util.Scanner;
 
 public class Isa {
+    private static final int MAX_TASK_COUNT = 100;
+    private static final String DIVIDER =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        Task[] tasks = new Task[100];
+        Task[] tasks = new Task[MAX_TASK_COUNT];
         int taskCount = 0;
 
         System.out.println("Helloo! I'm Isa");
         System.out.println("How can I help you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(DIVIDER);
 
         while (true) {
             String command = scanner.nextLine();
-            System.out.println("____________________________________________________________");
+            System.out.println(DIVIDER);
 
             if (command.equals("bye")) {
-                System.out.println("Bye. Hope you have a nice day!");
-                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope you have a nice day!");
+                System.out.println(DIVIDER);
                 break;
             } else if (command.equals("list")) {
                 System.out.println(" Here are the tasks in your list:");
@@ -26,24 +30,69 @@ public class Isa {
                     System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
             } else if (command.startsWith("mark ")) {
-                int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+                int taskIndex =
+                        Integer.parseInt(command.substring(5)) - 1;
+
                 tasks[taskIndex].markAsDone();
 
-                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println(
+                        " Nice! I've marked this task as done:");
                 System.out.println("   " + tasks[taskIndex]);
             } else if (command.startsWith("unmark ")) {
-                int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+                int taskIndex =
+                        Integer.parseInt(command.substring(7)) - 1;
+
                 tasks[taskIndex].markAsNotDone();
 
-                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println(
+                        " OK, I've marked this task as not done yet:");
                 System.out.println("   " + tasks[taskIndex]);
-            } else {
-                tasks[taskCount] = new Task(command);
-                System.out.println(" added: " + tasks[taskCount]);
+            } else if (command.startsWith("todo ")) {
+                String description = command.substring(5);
+
+                tasks[taskCount] = new Task(description);
                 taskCount++;
+
+                printTaskAdded(tasks[taskCount - 1], taskCount);
+            } else if (command.startsWith("deadline ")) {
+                String details = command.substring(9);
+                String[] parts = details.split(" /by ", 2);
+
+                String description = parts[0];
+                String by = parts[1];
+
+                tasks[taskCount] = new Task(description, by);
+                taskCount++;
+
+                printTaskAdded(tasks[taskCount - 1], taskCount);
+            } else if (command.startsWith("event ")) {
+                String details = command.substring(6);
+
+                int fromPosition = details.indexOf(" /from ");
+                int toPosition = details.indexOf(" /to ");
+
+                String description =
+                        details.substring(0, fromPosition);
+                String from = details.substring(
+                        fromPosition + 7, toPosition);
+                String to = details.substring(toPosition + 5);
+
+                tasks[taskCount] = new Task(description, from, to);
+                taskCount++;
+
+                printTaskAdded(tasks[taskCount - 1], taskCount);
             }
-            System.out.println("____________________________________________________________");
+
+            System.out.println(DIVIDER);
         }
+
         scanner.close();
+    }
+
+    private static void printTaskAdded(Task task, int taskCount) {
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + task);
+        System.out.println(
+                " Now you have " + taskCount + " tasks in the list.");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,36 @@
+// Task.java
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private final String description;
+    private final char type;
+    private final String by;
+    private final String from;
+    private final String to;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.type = 'T';
+        this.by = null;
+        this.from = null;
+        this.to = null;
+        this.isDone = false;
+    }
+
+    public Task(String description, String by) {
+        this.description = description;
+        this.type = 'D';
+        this.by = by;
+        this.from = null;
+        this.to = null;
+        this.isDone = false;
+    }
+
+    public Task(String description, String from, String to) {
+        this.description = description;
+        this.type = 'E';
+        this.by = null;
+        this.from = from;
+        this.to = to;
         this.isDone = false;
     }
 
@@ -21,6 +48,15 @@ public class Task {
 
     @Override
     public String toString() {
-        return "[" + getStatusIcon() + "] " + description;
+        String taskDetails = "[" + type + "][" + getStatusIcon()
+                + "] " + description;
+
+        if (type == 'D') {
+            taskDetails += " (by: " + by + ")";
+        } else if (type == 'E') {
+            taskDetails += " (from: " + from + " to: " + to + ")";
+        }
+
+        return taskDetails;
     }
 }
